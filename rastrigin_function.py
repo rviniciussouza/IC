c293f6424a4ff441f09d720500a70a93724a0f2d
Vinicius Rodrigues <r.viniciussouza@outlook.com>
2018-10-02 08:36:19 -0300

    add rastrigin

---
+++ rastrigin_function.py
@@ -0,0 +1,18 @@
+
+# Equipe: Vinicius Rodrigues
+#         Marcus Magalhaes
+#         Daniel Veloso Braga
+
+from scipy.optimize import differential_evolution
+import numpy as np
+
+def rastrigin(x):
+        somatorio = 10 * len(x)
+        for i in range(len(x)):
+            somatorio += (x[i] ** 2) - (10 * np.cos(2 * np.pi * x[i]))    
+        return somatorio
+
+limites = [(-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12)]
+result = differential_evolution(rastrigin, limites)
+print(result.x)
+print(result.fun)
