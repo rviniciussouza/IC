3d6a09684ffc6738bd10388635f75e5f9f0dd123
Vinicius Rodrigues <r.viniciussouza@outlook.com>
2018-10-02 08:48:21 -0300

    edit rastrigin

--- rastrigin_function.py
+++ rastrigin_function.py
@@ -12,6 +12,8 @@ def rastrigin(x):
             somatorio += (x[i] ** 2) - (10 * np.cos(2 * np.pi * x[i]))    
         return somatorio
 
+print(rastrigin([0,0,0,0]))
+
 limites = [(-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12), (-5.12, 5.12)]
 result = differential_evolution(rastrigin, limites)
 print(result.x)
